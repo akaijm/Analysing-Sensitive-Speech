5749d0830d575363b9a8f8169cc5086801315059
shirleyow <shirleyowwh@gmail.com>
2021-10-28 16:10:51 +0800

    fixed tabs issue

--- index.py
+++ index.py
@@ -16,64 +16,57 @@ from app import app
 from apps import topic_modeling, desc_analyses, time_series, sentiment_analysis, ecdf, agg_network, post_cent_network, absa, emotion_classif
 
 # Default dataset
-text_df = pd.read_csv("outputs/text_data.csv") # can change this dataset, just using it for the labels!
+# can change this dataset, just using it for the labels!
+text_df = pd.read_csv("outputs/text_data.csv")
 unique_labs = list(text_df['pred_label'].unique())
 unique_labs.sort()
 unique_labs.insert(0, "all")
 
 app.layout = html.Div([
-    dcc.Tabs(id='tabs-example', value='Overall', children=[
-        dcc.Tab(label='Overall', value='Overall'),
-        dcc.Tab(label='Network Graphs', value='Network Graphs'),
+    dcc.Tabs(id='tabs_dashboard', value='Overall', children=[
+        dcc.Tab(
+            html.Div([
+                html.H2("Dashboard for Analyzing Sensitive Speech",
+                        style={'textAlign': 'center'}),
+                html.Div([html.Label("Select a Label:"),
+                      dcc.Dropdown(
+                    id='selected_label',
+                    options=[{'label': i.capitalize(), 'value': i}
+                         for i in unique_labs],
+                    multi=False,
+                    clearable=False,
+                    value='all'
+                )]),
+                html.Div([desc_analyses.layout], className="mt-3"),
+                html.Div([topic_modeling.layout], className="mt-2"),
+                dbc.Row([
+                    dbc.Col([time_series.layout]),
+                    dbc.Col([sentiment_analysis.layout])
+                ], className="mt-3"),
+                html.Div([ecdf.layout], className="mt-3"),
+                dbc.Row([
+                    dbc.Col([absa.layout]),
+                    dbc.Col([emotion_classif.layout])
+                ], className="mt-3"),
+                html.Br()
+            ], style={
+                "paddingLeft": '30px',
+                "paddingRight": '30px',
+                "padding-top": '30px'
+            }), label='Overall', value='Overall'),
+        dcc.Tab(
+            html.Div([
+                html.H2("Dashboard for Analyzing Sensitive Speech",
+                        style={'textAlign': 'center'}),
+                html.Div([agg_network.layout], className="mt-3"),
+                html.Div([post_cent_network.layout], className="mt-3")], style={
+                "paddingLeft": '30px',
+                    "paddingRight": '30px',
+                    "padding-top": '30px'
+            }), label='Network Graphs', value='Network Graphs'),
     ]),
     html.Div(id='tabs-example-content-1')
 ])
 
-@app.callback(
-    Output('tabs-example-content-1', 'children'),
-    Input('tabs-example', 'value')
-)
-def render_content(tab):
-    if tab == 'Overall':
-        return html.Div([
-            html.H2("Dashboard for Analyzing Sensitive Speech",
-                    style={'textAlign': 'center'}),
-            html.Div([html.Label("Select a Label:"),
-                    dcc.Dropdown(
-                id='selected_label',
-                options=[{'label': i.capitalize(), 'value': i}
-                        for i in unique_labs],
-                multi=False,
-                clearable=False,
-                value='all'
-            )]),
-            html.Div([desc_analyses.layout], className="mt-3"),
-            html.Div([topic_modeling.layout], className="mt-2"),
-            dbc.Row([
-                dbc.Col([time_series.layout]),
-                dbc.Col([sentiment_analysis.layout])
-            ], className="mt-3"),
-            html.Div([ecdf.layout], className="mt-3"),
-            dbc.Row([
-                dbc.Col([absa.layout]),
-                dbc.Col([emotion_classif.layout])
-            ], className="mt-3"),
-            html.Br()
-        ], style={
-            "paddingLeft": '30px',
-            "paddingRight": '30px',
-            "padding-top": '30px'
-        })
-    elif tab == 'Network Graphs':
-        return html.Div([
-            html.H2("Dashboard for Analyzing Sensitive Speech",
-                    style={'textAlign': 'center'}),
-            html.Div([agg_network.layout], className="mt-3"),
-            html.Div([post_cent_network.layout], className="mt-3")] , style={
-                    "paddingLeft": '30px',
-                    "paddingRight": '30px',
-                    "padding-top": '30px'
-                })
-
 if __name__ == '__main__':
-    app.run_server(debug=True,dev_tools_ui=False)
+    app.run_server(debug=True)
